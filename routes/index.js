307833cdb96c85ddfff18aa514b28ff405ee943a
Guillaume <guillaumec.dev@gmail.com>
2023-07-26 15:01:38 +0200

    feat: add routes

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const userRoutes = require('./user.routes');
+const authRoutes = require('./auth.routes');
+const router = require('express').Router();
+
+
+router.use('/users', userRoutes);
+router.use('/auth', authRoutes);
+
+router.get('/',  (req, res) => {
+  res.render('index', { user: req.user });
+});
+
+module.exports = router;
