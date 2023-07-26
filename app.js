f5a998943b6bde24c582b22d2c713b908c5c3719
Guillaume <guillaumec.dev@gmail.com>
2023-07-26 16:09:40 +0200

    feat: add middleware, auth controller and imports

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const app = express();
 
 exports.app = app;
 
+require('./config/session.config');
+require('./config/passport.config');
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
